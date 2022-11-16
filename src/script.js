c4cdec86e23ad87edfb8ecd3c6a7b8ca55025b3f
Desire <105701137+desire39@users.noreply.github.com>
2022-11-16 00:49:43 +0000

    user accunt dashboard

---
+++ src/script.js
@@ -0,0 +1,152 @@
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+allSideMenu.forEach(item=> {
+	const li = item.parentElement;
+
+	item.addEventListener('click', function () {
+		allSideMenu.forEach(i=> {
+			i.parentElement.classList.remove('active');
+		})
+		li.classList.add('active');
+	})
+});
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function () {
+	sidebar.classList.toggle('hide');
+})
+
+
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function (e) {
+	if(window.innerWidth < 576) {
+		e.preventDefault();
+		searchForm.classList.toggle('show');
+		if(searchForm.classList.contains('show')) {
+			searchButtonIcon.classList.replace('bx-search', 'bx-x');
+		} else {
+			searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		}
+	}
+})
+
+if(window.innerWidth < 768) {
+	sidebar.classList.add('hide');
+} else if(window.innerWidth > 576) {
+	searchButtonIcon.classList.replace('bx-x', 'bx-search');
+	searchForm.classList.remove('show');
+}
+
+window.addEventListener('resize', function () {
+	if(this.innerWidth > 576) {
+		searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		searchForm.classList.remove('show');
+	}
+})
+
+const switchMode = document.getElementById('switch-mode');
+
+switchMode.addEventListener('change', function () {
+	if(this.checked) {
+		document.body.classList.add('dark');
+	} else {
+		document.body.classList.remove('dark');
+	}
+})
+
+
+// acctBalance start
+
+const acctBalanceLbl = document.getElementById("acctBalanceLbl");
+const deposits = [];
+const withdrawals = [];
+let totalBalance = 34010.00;
+const userDeposit = document.getElementById("userDeposit");
+const btnDeposit = document.getElementById("btnDeposit");
+const userWithdraw = document.getElementById("userWithdraw");
+const btnWithdraw = document.getElementById("btnWithdraw");
+
+// Create our number formatter.
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    /* 
+    the default value for minimumFractionDigits depends on the currency
+    and is usually already 2
+    */
+  });
+
+// accept deposits from user, store deposits in array
+btnDeposit.addEventListener('click', () => {
+
+    // checks if deposit is a number
+    if (isNaN(userDeposit.value)) {
+        alert("Please enter a number.");
+        return userDeposit.value = '';
+    } else {
+
+    // checks if deposit meets parameters
+        if (userDeposit.value < 0.01 || userDeposit.value > 10000) {
+            alert("You can only deposit between $0.01 and $10,000.")
+            return userDeposit.value = '';
+        } else {
+    
+        // push deposit to array
+        deposits.push(Number(userDeposit.value));
+        // calculate Total Balance
+        totalBalance += (Number(userDeposit.value));
+
+        // format TotalBalance to show $ XX.XX (2 decimal places)
+        let totalBalanceFormatted = formatter.format(totalBalance);
+        document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
+
+    // print deposit to console to verify success
+    console.log("$" + userDeposit.value);
+    return userDeposit.value = '';
+    }
+}
+    
+});
+
+// accept withdrawals from user, store withdrawals in array
+btnWithdraw.addEventListener('click', () => {
+
+    // checks if withdrawal is a number
+    if (isNaN(userWithdraw.value)) {
+        alert("Please enter a number.");
+        return userWithdraw.value = '';
+    } else {
+
+        // checks if withdrawal meets parameters
+        if (userWithdraw.value > totalBalance -5) {
+            alert("Your total balance cannot drop below $5.");
+            return userWithdraw.value = '';
+        } else {
+
+        // push withdrawal to array
+        withdrawals.push(Number(userWithdraw.value));
+        // calculate Total Balance
+        totalBalance -= (Number(userWithdraw.value));
+
+        // format TotalBalance to show $ XX.XX (2 decimal places)
+        let totalBalanceFormatted = formatter.format(totalBalance);
+        document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
+
+    // print withdrawal to console to verfify success
+    console.log("$" + userWithdraw.value);
+    return userWithdraw.value = '';
+    }
+}
+});
+
+// format TotalBalance to show $ XX.XX (2 decimal places)
+
+let totalBalanceFormatted = formatter.format(totalBalance);
+document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
