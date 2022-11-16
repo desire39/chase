d5198e332c0c2310549a0ca65e57899538d197bb
Desire <105701137+desire39@users.noreply.github.com>
2022-11-16 20:50:26 +0000

    User Account Dashboard

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,78 @@
 
 
 
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+allSideMenu.forEach(item=> {
+	const li = item.parentElement;
+
+	item.addEventListener('click', function () {
+		allSideMenu.forEach(i=> {
+			i.parentElement.classList.remove('active');
+		})
+		li.classList.add('active');
+	})
+});
+
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function () {
+	sidebar.classList.toggle('hide');
+})
+
+
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function (e) {
+	if(window.innerWidth < 576) {
+		e.preventDefault();
+		searchForm.classList.toggle('show');
+		if(searchForm.classList.contains('show')) {
+			searchButtonIcon.classList.replace('bx-search', 'bx-x');
+		} else {
+			searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		}
+	}
+})
+
+if(window.innerWidth < 768) {
+	sidebar.classList.add('hide');
+} else if(window.innerWidth > 576) {
+	searchButtonIcon.classList.replace('bx-x', 'bx-search');
+	searchForm.classList.remove('show');
+}
+
+window.addEventListener('resize', function () {
+	if(this.innerWidth > 576) {
+		searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		searchForm.classList.remove('show');
+	}
+})
+
+const switchMode = document.getElementById('switch-mode');
+
+switchMode.addEventListener('change', function () {
+	if(this.checked) {
+		document.body.classList.add('dark');
+	} else {
+		document.body.classList.remove('dark');
+	}
+})
+
+
+
+
 // acctBalance start
 
 const acctBalanceLbl = document.getElementById("acctBalanceLbl");
 const deposits = [];
 const withdrawals = [];
-let totalBalance = 1000;
+let totalBalance = 34010.00;
 const userDeposit = document.getElementById("userDeposit");
 const btnDeposit = document.getElementById("btnDeposit");
 const userWithdraw = document.getElementById("userWithdraw");
@@ -66,7 +132,7 @@ btnWithdraw.addEventListener('click', () => {
     } else {
 
         // checks if withdrawal meets parameters
-        if (userWithdraw.value > totalBalance - 5) {
+        if (userWithdraw.value > totalBalance -5) {
             alert("Your total balance cannot drop below $5.");
             return userWithdraw.value = '';
         } else {
@@ -91,3 +157,5 @@ btnWithdraw.addEventListener('click', () => {
 
 let totalBalanceFormatted = formatter.format(totalBalance);
 document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
+
+
