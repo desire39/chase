616d60c21c2e603c65e54e37236ff8ecd738f3b5
Desire <105701137+desire39@users.noreply.github.com>
2022-11-18 15:22:55 +0000

    Admin Dashboard

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,121 +1,40 @@
-// add hovered class to selected list item
-let list = document.querySelectorAll(".navigation li");
+'use strict';
 
-function activeLink() {
-  list.forEach((item) => {
-    item.classList.remove("hovered");
+
+/**
+ * toggle active class on header
+ * when clicked nav-toggle-btn
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-menu-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("active");
+});
+
+
+
+/**
+ * toggle ctx-menu when click on card-menu-btn
+ */
+
+const menuBtn = document.querySelectorAll("[data-menu-btn]");
+
+for (let i = 0; i < menuBtn.length; i++) {
+  menuBtn[i].addEventListener("click", function () {
+    this.nextElementSibling.classList.toggle("active");
   });
-  this.classList.add("hovered");
 }
 
-list.forEach((item) => item.addEventListener("mouseover", activeLink));
-
-// Menu Toggle
-let toggle = document.querySelector(".toggle");
-let navigation = document.querySelector(".navigation");
-let main = document.querySelector(".main");
-
-toggle.onclick = function () {
-  navigation.classList.toggle("active");
-  main.classList.toggle("active");
-};
 
 
+/**
+ * load more btn loading spin toggle
+ */
 
-// ---------- CHARTS ----------
+const loadMoreBtn = document.querySelector("[data-load-more]");
 
-// BAR CHART
-var barChartOptions = {
-  series: [{
-    data: [10, 8, 6, 4, 2]
-  }],
-  chart: {
-    type: 'bar',
-    height: 350,
-    toolbar: {
-      show: false
-    },
-  },
-  colors: [
-    "#246dec",
-    "#cc3c43",
-    "#367952",
-    "#f5b74f",
-    "#4f35a1"
-  ],
-  plotOptions: {
-    bar: {
-      distributed: true,
-      borderRadius: 4,
-      horizontal: false,
-      columnWidth: '40%',
-    }
-  },
-  dataLabels: {
-    enabled: false
-  },
-  legend: {
-    show: false
-  },
-  xaxis: {
-    categories: ["Laptop", "Phone", "Monitor", "Headphones", "Camera"],
-  },
-  yaxis: {
-    title: {
-      text: "Count"
-    }
-  }
-};
-
-var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
-barChart.render();
-
-
-// AREA CHART
-var areaChartOptions = {
-  series: [{
-    name: 'Purchase Orders',
-    data: [31, 40, 28, 51, 42, 109, 100]
-  }, {
-    name: 'Sales Orders',
-    data: [11, 32, 45, 32, 34, 52, 41]
-  }],
-  chart: {
-    height: 350,
-    type: 'area',
-    toolbar: {
-      show: false,
-    },
-  },
-  colors: ["#4f35a1", "#246dec"],
-  dataLabels: {
-    enabled: false,
-  },
-  stroke: {
-    curve: 'smooth'
-  },
-  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-  markers: {
-    size: 0
-  },
-  yaxis: [
-    {
-      title: {
-        text: 'Purchase Orders',
-      },
-    },
-    {
-      opposite: true,
-      title: {
-        text: 'Sales Orders',
-      },
-    },
-  ],
-  tooltip: {
-    shared: true,
-    intersect: false,
-  }
-};
-
-var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
-areaChart.render();
+loadMoreBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+});
