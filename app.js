fcb2178413269c9039330ecc65830c90015c4cd8
desire39 <desire39@gmail.com>
2022-12-29 16:43:54 +0000

    user signup

--- app.js
+++ app.js
@@ -1,76 +1,64 @@
-const express = require('express');
-const http = require('http');
 const bcrypt = require('bcrypt');
-const path = require("path");
-const bodyParser = require('body-parser');
-const users = require('./data').userDB;
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const auth = require('./auth/authenticate');
 
 const app = express();
-const server = http.createServer(app);
+const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,'./public')));
+const users = [];
 
+app.use(express.json());
 
-app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname,'./public/index.html'));
+app.get('/', (req, res) => {
+  res.send('users registration!')
 });
 
-
-app.post('/register', async (req, res) => {
-    try{
-        let foundUser = users.find((data) => req.body.email === data.email);
-        if (!foundUser) {
-    
-            let hashPassword = await bcrypt.hash(req.body.password, 10);
-    
-            let newUser = {
-                id: Date.now(),
-                username: req.body.username,
-                email: req.body.email,
-                password: hashPassword,
-            };
-            users.push(newUser);
-            console.log('User list', users);
-    
-            res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
-        } else {
-            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
-        }
-    } catch{
-        res.send("Internal server error");
-    }
+app.get('/users', auth, (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'register a users.',
+        data: {
+          user: {
+              email: req.user.email,
+          },
+        },
+      });
 });
 
-app.post('/login', async (req, res) => {
-    try{
-        let foundUser = users.find((data) => req.body.email === data.email);
-        if (foundUser) {
-    
-            let submittedPass = req.body.password; 
-            let storedPass = foundUser.password; 
-    
-            const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
-            if (passwordMatch) {
-                let usrname = foundUser.username;
-                res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
-            } else {
-                res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
-            }
+app.post('/auth/v1/register', async (req, res) => {
+    try {
+        if (users.some(user => user.email === req.body.email)) {
+            const err = new Error('Email Taken!')
+            err.status = 400;
+            throw err;
         }
-        else {
-    
-            let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
-            await bcrypt.compare(req.body.password, fakePass);
-    
-            res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
+
+        const user = {
+            email: req.body.email,
+            password: await bcrypt.hash(req.body.password, 12),
         }
-    } catch{
-        res.send("Internal server error");
-    }
+
+        users.push(user);
+
+        res.status(201).json({
+          status: 'success',
+          message: 'User Registered!',
+          data: {
+            user: {
+                email: user.email,
+            },
+          },
+        });
+      } catch (err) {
+        res.status(err.status).json({
+            status: 'fail',
+            message: err.message,
+          });
+      }
 });
 
-
-app.listen(4000, ()=> {
-    console.log("server started on port 4000")
-})
+app.listen(port, () => {
+  console.log(`app running on http://127.0.0.1:${port}`)
+});
